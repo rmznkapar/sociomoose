414aeed031f97e50fb7fde6fa5bb4776be14fbde
rmznkapar <rmznkapar@gmail.com>
2021-05-29 15:03:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 const authRouter = require('./routes/authRouter.js');
 const postRouter = require('./routes/postRouter.js');
